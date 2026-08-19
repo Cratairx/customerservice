fd4721e14fa82abb53dbb57255944ba51e2434ab
IsacAstr <strowe1000@gmail.com>
2026-08-19 12:38:44 +0200

    par uppdaterat POM fil Isac/Daniel/Emil

--- src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -12,3 +12,15 @@ public class CustomerServiceApplication {
 //
     //
 }
+/*Kundtjänsten ska vara ett helt nytt Spring Bootprojekt (en egen applikation)
+Kundtjänsten ska ha en EGEN databasv(OBS SQLITE/H2 EJ TILLÅTET!!)
+Kundtjänsten ansvarar för ALL kundhantering: registrera, ändra och ta bort kunder
+Kundtjänsten ska ha ett REST API som svarar med JSON, minst:
+hämta en kund
+hämta alla kunder (ENBART OM NI HAR EN ADMIN SIDA, ANNARS BORTSE FRÅN DENNA PUNKT!)
+registrera en kund
+ändra en kunds uppgifter
+ta bort en kund
+Regeln från Backend 1 gäller fortfarande: en kund får bara tas bort om kunden inte har några aktiva bokningar
+Men nu ligger bokningarna i en annan tjänst! Kundtjänsten måste alltså FRÅGA bokningstjänsten om kunden har aktiva bokningar innan kunden tas bort
+ */
