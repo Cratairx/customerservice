4df0f87ac033ad3cc4ca9f2df0afb5ecbf2d5510
Cratairx <daniel.inserte@gmail.com>
2026-08-31 14:47:54 +0200

    fixat i CustomerServiceImpl

--- src/main/java/org/example/customerservice/services/CustomerServiceImpl.java
+++ src/main/java/org/example/customerservice/services/CustomerServiceImpl.java
@@ -64,7 +64,7 @@ public class CustomerServiceImpl implements CustomerService {
 
         Boolean hasBooking;
         try {
-            hasBooking = restTemplate.getForObject("http://localhost:8081/api/bookings/?customerId={id}" + id, Boolean.class);
+            hasBooking = restTemplate.getForObject("http://localhost:8081/api/bookings/getbooking/{id}" + id, Boolean.class);
         } catch (RestClientException e) {
             return false;
         }
