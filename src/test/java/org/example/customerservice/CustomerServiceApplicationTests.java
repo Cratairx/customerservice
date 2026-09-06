73518add6c79e4e87e2b0a5f5af67b238b524988
Cratairx <daniel.inserte@gmail.com>
2026-09-06 11:45:46 +0200

    Fixat med naming stuff

--- src/test/java/org/example/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/org/example/customerservice/CustomerServiceApplicationTests.java
@@ -1,11 +1,37 @@
 package org.example.customerservice;
 
+import org.example.customerservice.model.Customer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
+
+
 class CustomerServiceApplicationTests {
 
+    @Test
+    void getCustomerByFirstName(){
+        RestTemplate restTemplate = new RestTemplate();
+        MockRestServiceServer server = MockRestServiceServer.createServer(restTemplate);
+
+        server.expect(requestTo("http://customer-service/customer"))
+                .andRespond(withSuccess(
+                        "{\"id\":1,\"firstName\":\"Daniel\",\"lastName\":\"Inserte\"}",
 
 
+                        MediaType.APPLICATION_JSON));
+        Customer customer = restTemplate.getForObject("http://customer-service/customer", Customer.class);
+        assertNotNull(customer);
+        assertEquals("Daniel",customer.getFirstName());
+
+}
 }
